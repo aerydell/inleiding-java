36d7e7fd1a866bd7028953faf06876d9745d40e3
aerydell <Stevenklijnsma95>
2018-10-29 15:25:34 +0100

    initial code

---
+++ src/h02/Opdracht2Part1.java
@@ -0,0 +1,29 @@
+package h02;
+
+/*
+@(#)Show.java 1.0 03/02/26
+
+*
+
+Voorbeeld 2.1
+
+*
+
+*/
+
+//importeer de benodigde klassen uit de Java bibliotheek
+import java.awt.*;
+                import java.applet.*;
+
+//een klasse met de naam Show van het type Applet
+public class Opdracht2Part1 extends Applet {
+    //een (lege) methode die de Applet initialiseert
+    public void init() {
+        setBackground(Color.blue);
+    }
+    //een methode die de inhoud van het scherm tekent
+    public void paint(Graphics g) {
+        g.setColor(Color.yellow);
+        g.drawString("Steven Klijnsma", 50, 60 );
+    }
+}
