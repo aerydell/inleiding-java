47c616b9489326d882649530237dc5d654c31b40
aerydell <Stevenklijnsma95>
2018-10-30 09:09:23 +0100

    initial code

--- src/h04/Opdracht4Deel1.java
+++ src/h04/Opdracht4Deel1.java
@@ -1,4 +1,16 @@
 package h04;
 
-public class Opdracht4Deel1 {
+import java.awt.*;
+        import java.applet.*;
+
+public class Opdracht4Deel1 extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        g.drawLine(20,20,120,20);
+        g.drawLine(20,20,70,105);
+        g.drawLine(120,20,70,105);
+    }
 }
