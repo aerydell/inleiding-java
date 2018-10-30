00ec4ac1176dc030d11a615c70b63287c29e28c1
aerydell <Stevenklijnsma95>
2018-10-30 09:11:07 +0100

    changed the names of the .java files to make them easier to follow

--- src/h02/Opdracht1.java
+++ src/h02/Opdracht1.java
@@ -13,18 +13,17 @@ Voorbeeld 2.1
 
 //importeer de benodigde klassen uit de Java bibliotheek
 import java.awt.*;
-import java.applet.*;
+                import java.applet.*;
 
 //een klasse met de naam Show van het type Applet
-public class Opdracht2Part2 extends Applet {
+public class Opdracht1 extends Applet {
     //een (lege) methode die de Applet initialiseert
     public void init() {
+        setBackground(Color.blue);
     }
     //een methode die de inhoud van het scherm tekent
     public void paint(Graphics g) {
-        g.setColor(Color.blue);
-        g.drawString("Steven", 50, 60 );
-        g.setColor(Color.red);
-        g.drawString("Klijnsma", 50, 75);
+        g.setColor(Color.yellow);
+        g.drawString("Steven Klijnsma", 50, 60 );
     }
 }
--- src/h02/Opdracht2.java
+++ src/h02/Opdracht2.java
@@ -13,17 +13,18 @@ Voorbeeld 2.1
 
 //importeer de benodigde klassen uit de Java bibliotheek
 import java.awt.*;
-                import java.applet.*;
+import java.applet.*;
 
 //een klasse met de naam Show van het type Applet
-public class Opdracht2Part1 extends Applet {
+public class Opdracht2 extends Applet {
     //een (lege) methode die de Applet initialiseert
     public void init() {
-        setBackground(Color.blue);
     }
     //een methode die de inhoud van het scherm tekent
     public void paint(Graphics g) {
-        g.setColor(Color.yellow);
-        g.drawString("Steven Klijnsma", 50, 60 );
+        g.setColor(Color.blue);
+        g.drawString("Steven", 50, 60 );
+        g.setColor(Color.red);
+        g.drawString("Klijnsma", 50, 75);
     }
 }
