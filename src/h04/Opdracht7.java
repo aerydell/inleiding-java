eecb967ea70fe9e955e6f50b08259c5bd0bb3e07
aerydell <Stevenklijnsma95>
2018-10-30 13:08:52 +0100

    finished 4.7

--- src/h04/Opdracht7.java
+++ src/h04/Opdracht7.java
@@ -6,6 +6,10 @@ public class Opdracht7 extends Applet {
     public void init() {
     }
     public void paint(Graphics g){
-
+        g.drawRoundRect(20,20,50,50,10,10);
+        g.fillOval(32,32,5,5);
+        g.fillOval(32,53,5,5);
+        g.fillOval(53,32,5,5);
+        g.fillOval(53,53,5,5);
     }
 }
