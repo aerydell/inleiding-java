3f3e4f50f1f040a3d86eb4eb4bca8506bec4ec08
aerydell <Stevenklijnsma95>
2018-10-31 09:09:34 +0100

    finished 5.2

--- src/h05/Opracht2.java
+++ src/h05/Opracht2.java
@@ -10,7 +10,7 @@ public class Opracht2 extends Applet {
     int GewichtHans;
     int GewichtValerie;
     int GewichtJeroen;
-    int breedte;
+    int hoogte;
 
     public void init(){
         Hans = Color.MAGENTA;
@@ -19,30 +19,41 @@ public class Opracht2 extends Applet {
         GewichtHans = 80;
         GewichtJeroen = 100;
         GewichtValerie = 40;
-        breedte = 20;
+        hoogte = 20;
 
     }
     public void paint(Graphics g){
-        g.drawLine(40,40,40,200);
-        g.drawLine(40,40,180,40);
-        g.drawLine(35,180,40,180);
-        g.drawLine(35,160,40,160);
-        g.drawLine(35,140,40,140);
-        g.drawLine(35,120,40,120);
-        g.drawLine(35,100,40,100);
-        g.drawLine(35,80,40,80);
-        g.drawLine(35,60,40,60);
-        g.drawLine(35,40,40,40);
-        g.drawString("160",13,200);
-        g.drawString("140",13,180);
-        g.drawString("120",13,160);
-        g.drawString("100",13,140);
-        g.drawString("80",20,120);
-        g.drawString("60",20,100);
-        g.drawString("40",20,80);
-        g.drawString("20",20,60);
-        g.drawString("0",27,40);
+        g.drawLine(40,60,40,200);
+        g.drawLine(40,200,200,200);
+        g.drawLine(60,205,60,200);
+        g.drawLine(80,205,80,200);
+        g.drawLine(100,205,100,200);
+        g.drawLine(120,205,120,200);
+        g.drawLine(140,205,140,200);
+        g.drawLine(160,205,160,200);
+        g.drawLine(180,205,180,200);
+        g.drawLine(200,205,200,200);
+        g.drawString("160",200,220);
+        g.drawString("140",175,220);
+        g.drawString("120",150,220);
+        g.drawString("100",125,220);
+        g.drawString("80",110,220);
+        g.drawString("60",90,220);
+        g.drawString("40",70,220);
+        g.drawString("20",50,220);
+        g.drawString("0",30,220);
+        g.drawString("valerie",60,250);
+        g.drawString("hans",60,270);
+        g.drawString("jeroen",60,290);
+        g.drawString("gewicht in kilo's",80,40);
         g.setColor(Valerie);
-        g.fillRect(60,40,breedte,GewichtValerie);
+        g.fillRect(41,160,GewichtValerie,hoogte);
+        g.fillRect(40,240,10,10);
+        g.setColor(Hans);
+        g.fillRect(41,80,GewichtHans,hoogte);
+        g.fillRect(40,260,10,10);
+        g.setColor(Jeroen);
+        g.fillRect(41,120,GewichtJeroen,hoogte);
+        g.fillRect(40,280,10,10);
     }
 }
