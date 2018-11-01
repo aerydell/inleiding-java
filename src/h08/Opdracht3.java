dd0ae3c521fad7925d2c349102593cd27779b669
aerydell <Stevenklijnsma95>
2018-11-01 11:36:37 +0100

    h8 afgerond

--- src/h08/Opdracht3.java
+++ src/h08/Opdracht3.java
@@ -6,29 +6,42 @@ import java.awt.*;
 import java.applet.*;
 import java.awt.event.*;
 
-public class knop extends Applet {
+public class Opdracht3 extends Applet {
     TextField tekstvak;
     Label label;
     double getal;
+    Button knop;
 
     public void init() {
         tekstvak = new TextField("", 20);
         label = new Label("Type een getal");
         tekstvak.addActionListener( new TekstvakListener() );
+        knop = new Button("OK");
+        KnopListener kl = new KnopListener();
+        knop.addActionListener( kl );
         add(label);
         add(tekstvak);
+        add(knop);
     }
 
     public void paint(Graphics g) {
-        g.drawString("Het getal is " + getal, 50, 60 );
+        g.drawString("bedrag inclusief btw = " + getal, 50, 60 );
+    }
+
+    class KnopListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String s = tekstvak.getText();
+            getal = Double.parseDouble( s );
+            getal = getal * 1.21;
+            repaint();
+        }
     }
 
     class TekstvakListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             String s = tekstvak.getText();
             getal = Double.parseDouble( s );
-            //Je kan deze regels ook samenvoegen
-            //getal = Double.parseDouble(tekstvak.getText());
+            getal = getal * 1.21;
             repaint();
         }
     }
---
+++ src/h08/PraktijkOpdracht.java
@@ -0,0 +1,91 @@
+package h08;
+
+import javax.swing.*;
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+
+public class PraktijkOpdracht extends Applet {
+    TextField tekstvak1, tekstvak2;
+    Button plus;
+    Button min;
+    Button Divide;
+    Button multiply;
+    double getal;
+
+    public void init() {
+        tekstvak1 = new TextField("",10);
+        tekstvak2 = new TextField("",10);
+        plus = new Button("+");
+        min = new Button("-");
+        Divide = new Button("/");
+        multiply = new Button("*");
+//        tekstvak1.addActionListener( new Tekstvak1Listener() );
+//        tekstvak2.addActionListener(new Tekstvak2Listener() );
+        PlusListener pl = new PlusListener();
+        MinListener ml = new MinListener();
+        DivideListener dl = new DivideListener();
+        MultiplyListener xl = new MultiplyListener();
+        plus.addActionListener( pl );
+        min.addActionListener( ml );
+        Divide.addActionListener( dl );
+        multiply.addActionListener( xl );
+        add(tekstvak1);
+        add(tekstvak2);
+        add(plus);
+        add(min);
+        add(Divide);
+        add(multiply);
+    }
+
+    public void paint(Graphics g) {
+
+    }
+
+    public class PlusListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String s = tekstvak1.getText();
+            double arg1 = Double.parseDouble(s);
+            double arg2 = Double.parseDouble(tekstvak2.getText());
+            double uitkomst = arg1 + arg2;
+            tekstvak1.setText(String.valueOf(uitkomst));
+            tekstvak2.setText("");
+        }
+    }
+
+    public class MinListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            double arg1 = Double.parseDouble(tekstvak1.getText());
+            double arg2 = Double.parseDouble(tekstvak2.getText());
+            double uitkomst = arg1 - arg2;
+            tekstvak1.setText(String.valueOf(uitkomst));
+            tekstvak2.setText("");
+        }
+    }
+
+    public class DivideListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            double arg1 = Double.parseDouble(tekstvak1.getText());
+            double arg2 = Double.parseDouble(tekstvak2.getText());
+            double uitkomst = arg1 / arg2;
+            tekstvak1.setText(String.valueOf(uitkomst));
+            tekstvak2.setText("");
+        }
+    }
+
+    public class MultiplyListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            double arg1 = Double.parseDouble(tekstvak1.getText());
+            double arg2 = Double.parseDouble(tekstvak2.getText());
+            double uitkomst = arg1 * arg2;
+            tekstvak1.setText(String.valueOf(uitkomst));
+            tekstvak2.setText("");
+        }
+    }
+}
+
