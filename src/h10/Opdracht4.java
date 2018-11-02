43342b61af4adbb4be8c4126214a401837fd0629
aerydell <Stevenklijnsma95>
2018-11-02 12:19:43 +0100

    finished 10.4 and opened 10.5

--- src/h10/Opdracht4.java
+++ src/h10/Opdracht4.java
@@ -3,37 +3,46 @@ import java.awt.*;
 import java.applet.*;
 import java.awt.event.*;
 
-public class Opdracht3 extends Applet{
-    int maand;
-    TextField tekstvak;
+public class Opdracht4 extends Applet {
+    int jaar, maand;
+    TextField tekstvak1, tekstvak2;
     Label label;
     String s, tekst;
 
-    public void init(){
-        tekstvak = new TextField("",2);
-        label = new Label("typ het maandnummer en druk enter.");
-        tekstvak.addActionListener(new TekstvakListener());
+    public void init() {
+        tekstvak1 = new TextField("", 2);
+        tekstvak2 = new TextField("", 4);
+        label = new Label("typ het maandnummer en jaartel en druk enter.");
+        tekstvak1.addActionListener(new Tekstvak1Listener());
+        tekstvak2.addActionListener(new Tekstvak1Listener());
         tekst = "";
         add(label);
-        add(tekstvak);
+        add(tekstvak1);
+        add(tekstvak2);
     }
 
     public void paint(Graphics g) {
-        g.drawString(tekst, 50,60);
+        g.drawString(tekst, 50, 60);
 
     }
 
-    class TekstvakListener implements ActionListener {
-        @Override
+
+    class Tekstvak1Listener implements ActionListener {
+
         public void actionPerformed(ActionEvent e) {
-            s = tekstvak.getText();
-            maand = Integer.parseInt( s );
-            switch(maand) {
+
+            s = tekstvak1.getText();
+            maand = Integer.parseInt(s);
+
+            s = tekstvak2.getText();
+            jaar = Integer.parseInt(s);
+
+            switch (maand) {
                 case 1:
                     tekst = "januari 31 dagen";
                     break;
                 case 2:
-                    tekst = "februari 28 dagen (29 in een schrikkeljaar)";
+                    tekst = "februari 28 dagen";
                     break;
                 case 3:
                     tekst = "maart 31 dagen";
@@ -69,7 +78,15 @@ public class Opdracht3 extends Applet{
                     tekst = "ongeldige maand";
                     break;
             }
-            repaint();
+            if ((jaar % 4 == 0 && !(jaar % 100 == 0)) || jaar % 400 == 0) {
+                switch (maand) {
+                    case 2:
+                        tekst = "februari 29 dagen";
+                        break;
+                }
+            }
+        repaint();
         }
     }
 }
+
