50df652eef87b85d69583b445603d89a223446a7
aerydell <Stevenklijnsma95>
2018-11-05 12:04:28 +0100

    finished 11.6 and 11.7

--- src/h11/Opdracht6.java
+++ src/h11/Opdracht6.java
@@ -1,4 +1,27 @@
 package h11;
 
-public class Opdracht6 {
+import java.awt.*;
+import java.applet.*;
+
+public class Opdracht6 extends Applet {
+
+    int teller = 100;
+    int x = 10;
+    int y = 10;
+
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+        while (teller > 19) {
+            y += 10;
+            x += 10;
+            g.drawOval(x, y, teller, teller);
+            teller = teller - 20;
+
+        }
+
+    }
 }
--- src/h11/Opdracht7.java
+++ src/h11/Opdracht7.java
@@ -1,4 +1,25 @@
 package h11;
 
-public class Opdracht7 {
+import java.awt.*;
+import java.applet.*;
+
+public class Opdracht7 extends Applet {
+    int teller = 500;
+    int y = 15;
+    int x = 15;
+
+    public void init(){
+
+    }
+
+    public void paint(Graphics g) {
+        while(teller>9) {
+            y += 5;
+            x += 5;
+            g.drawOval (x, y, teller, teller);
+            teller = teller - 10;
+
+        }
+    }
+
 }
