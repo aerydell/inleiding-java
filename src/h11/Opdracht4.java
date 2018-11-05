849cb4910e2d29b83ead457e64d2bc9c6c98bba9
aerydell <Stevenklijnsma95>
2018-11-05 11:42:30 +0100

    finished 11.4 and 11.5, and pre-added 6 through praktijkOpdracht

--- src/h11/Opdracht4.java
+++ src/h11/Opdracht4.java
@@ -1,4 +1,25 @@
 package h11;
 
-public class Opdracht4 {
+import java.awt.*;
+import java.applet.*;
+
+public class Opdracht4 extends Applet {
+
+    int teller = 1;
+    int arg1, table;
+    int y = 0;
+
+    public void init(){
+        table = 3;
+    }
+
+    public void paint(Graphics g) {
+        while(teller <= 11) {
+            y += 20;
+            g.drawString("" + arg1, 40, y);
+            arg1 = teller * table;
+            teller++;
+        }
+    }
+
 }
--- src/h11/Opdracht5.java
+++ src/h11/Opdracht5.java
@@ -1,4 +1,19 @@
 package h11;
 
-public class Opdracht5 {
+import java.awt.*;
+import java.applet.*;
+
+public class Opdracht5 extends Applet {
+
+    public void init(){
+
+    }
+
+    public void paint(Graphics g) {
+        g.drawRect(20,20,20,20);
+        g.drawRect(40,40,20,20);
+        g.drawRect(60,60,20,20);
+        g.drawRect(80,80,20,20);
+        g.drawRect(100,100,20,20);
+    }
 }
